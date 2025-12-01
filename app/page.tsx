1c0001d2d659fd3144aebc0a3e4eeff9d0493665
Alexandre NGUYEN TRUNG <alexrush@gmail.com>
2025-12-01 09:19:19 +0100

    feat(page): assemble home page with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
+import { Articles } from "@/components/sections/Articles";
+import { ContentSection } from "@/components/sections/ContentSection";
 import { Hero } from "@/components/sections/Hero";
+import { Statistics } from "@/components/sections/Statistics";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero />
+      <main className="relative">
+        <Hero />
+        <ContentSection />
+        <Articles />
+        <Statistics />
+      </main>
+      <Footer />
     </>
   );
 }
